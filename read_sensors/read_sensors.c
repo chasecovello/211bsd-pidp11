d8b2de753e0e7b002a067790bb36e36a2afe5313
Chase Covello <chase@chasecovello.com>
2019-09-09 12:20:48 -0700

    Added header comments and LICENSE file

--- read_sensors/read_sensors.c
+++ read_sensors/read_sensors.c
@@ -1,5 +1,33 @@
 /* read_sensors.c: read and output values of temperature and pressure
- * sensors from memory-mapped device */
+ * sensors from the BMP180 memory-mapped sensor device. See:
+ *
+ * https://obsolescence.wixsite.com/obsolescence/pidp-11-temp-barometer-hack
+ *
+ * for details on how to set this up on a PiDP-11.
+ *
+ * This program requires access to /dev/kmem to read the sensor data, so it
+ * must be run setgid kmem, or by a user with kmem group membership.
+ *
+ * Copyright (c) 2019 Chase Covello
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
 
 #include <sys/file.h>
 #include <sys/types.h>
